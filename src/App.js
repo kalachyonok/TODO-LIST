0f245ae0ee7ab4dcbc812d69307fe3edc558f97c
Olga Kalacheva <kalach2256954@gmail.com>
2023-11-02 19:08:57 +0400

    fix header and input-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
+import style from "./App.module.css";
 
 function App() {
   return (
-    <div>
+    <div className={style.wrap}>
       <Header />
       <Form />
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,14 @@
-import { Button } from "./Button";
 import style from "./Form.module.css";
 
 export const Form = () => {
   return (
-    <div className={style.form}>
-      <form>
-        {/* <label htmlFor="task"></label> */}
-        <input type="text" placeholder="What needs to be Done" />
-      </form>
-      <Button />
-    </div>
+    <form className={style.form}>
+      <input
+        className={`form-control ${style.input}`}
+        type="text"
+        placeholder="What needs to be Done"
+      />
+      <button className="btn btn-outline-secondary">Add</button>
+    </form>
   );
 };
