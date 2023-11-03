a334187ff33ed9f7a9f92f9bd763bf24729e8c52
Olga Kalacheva <kalach2256954@gmail.com>
2023-11-03 16:01:40 +0400

    add buttons into input component

--- src/DataItem.js
+++ src/DataItem.js
@@ -1,3 +1,17 @@
+import style from "./DataItem.module.css";
+
 export const DataItem = ({ title }) => {
-  return <li className="list-group-item">{title}</li>;
+  return (
+    <li className={`list-group-item ${style.li}`}>
+      {title}
+      <div className={style["btn-set"]}>
+        <button type="button" class="btn btn-outline-danger">
+          DEL
+        </button>
+        <button type="button" class="btn btn-outline-success">
+          !
+        </button>
+      </div>
+    </li>
+  );
 };
